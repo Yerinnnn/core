eeaccf0da187e22bf9467a2779883d6a964ef140
Yerinnnn <shinyerin0216@gmail.com>
2024-08-10 23:23:41 +0900

    Refactor: 새로운 할인 정책 적용과 문제점
    - DiscountPolicy 인터페이스뿐만 아니라 FixDiscountPolicy 구현체도 의존하고 있으니, "DIP 위반"
    - FixDiscountPolicy를 RateDiscountPolicy로, OrderServiceImpl를 수정해야하니, "OCP 위반"

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,19 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // DiscountPolicy 인터페이스뿐만 아니라 FixDiscountPolicy 구현체도 의존하고 있으니, "DIP 위반"
+    // FixDiscountPolicy를 RateDiscountPolicy로, OrderServiceImpl를 수정해야하니, "OCP 위반"
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 인터페이스에만 의존하도록 설계 수정
+    private DiscountPolicy discountPolicy;
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
