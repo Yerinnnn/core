dcae901cbea0fcb9e289c82fb3f266af75915f3b
Yerinnnn <shinyerin0216@gmail.com>
2024-08-10 18:42:48 +0900

    Test: 주문과 할인 도메인 실행과 테스트

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        // Primitive type으로 해도 되는데, 이후에 DB에 넣고 하려면 null을 넣어야할 수도 있어서 Primitive type 사용
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
