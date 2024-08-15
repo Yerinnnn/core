add21bffbda1b9d93f0018e6e56d0da3a8937d00
Yerinnnn <shinyerin0216@gmail.com>
2024-08-15 12:01:10 +0900

    Feat: 필터
    - `@Component` 면 충분하기 때문에, `includeFilters` 를 사용할 일은 거의 없음 (`excludeFilters` 는 여러가지 이유로 간혹 사용할 때가 있지만 많지는 않음)
    - 특히 최근 스프링 부트는 컴포넌트 스캔을 기본으로 제공하는데, 옵션을 변경하면서 사용하기보다는 스프링의 기본 설정에 최대한 맞추어 사용하는 것을 권장

---
+++ src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)  // TYPE : 타겟이 클래스 (클래스 레벨에 붙인다는 뜻)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
