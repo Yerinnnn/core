b7c78e2d72ae5028e0f4fbc6412d9dce0d4133a0
Yerinnnn <shinyerin0216@gmail.com>
2024-08-15 20:17:58 +0900

    Feat: 롬복과 최신 트랜드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,42 +1,16 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor  // lombok의 기능 : final 붙은 필수값을 가지고, 생성자를 자동으로 만들어줌 (cmd + F12로 확인 가능)
 public class OrderServiceImpl implements OrderService {
-    // 생성자 주입을 사용하면 또 좋은 점 : final 키워드 사용 가능
-    // 한 번 생성할 때 정해지면 안 바뀜
-    // 생성자에서만 값을 셋팅할 수 있음 (final은 원래, 초기값을 넣어주거나 생성자로 넣어주거나)
-    // 실수로 주입 안 해줘도, 컴파일 오류로 알려줌
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    // 수정자 의존관계인 경우
-//    private MemberRepository memberRepository;
-//    private DiscountPolicy discountPolicy;
-//
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        this.discountPolicy = discountPolicy;
-//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
