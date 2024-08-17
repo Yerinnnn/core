de6d245a99137e701f6da8ceea24e655db923e4b
Yerinnnn <shinyerin0216@gmail.com>
2024-08-18 00:28:49 +0900

    Feat: request 스코프 예제 만들기

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,25 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);  // 로그 남길 때, 내용에 [http://localhost:8080/log-demo]도 포함되도록
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
